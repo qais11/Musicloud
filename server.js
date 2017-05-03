0d202c2b926cb2e4185af7f5334a22dfd2759a80
Qais Malik <qais.gh11@gmail.com>
2017-05-03 17:23:44 -0500

    Changing port for production

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const {json} = require('body-parser');
 const cors = require('cors');
-const port = 3000;
+const port = 4000;
 
 app.use(express.static(__dirname + '/dist'))
 
